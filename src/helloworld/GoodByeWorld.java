634530ce04df429e5d74fde7e9c30a535a95606f
Steve Falcigno <steve1281@hotmail.com>
2015-08-04 11:37:57 -0400

    initial commit

---
+++ src/helloworld/GoodByeWorld.java
@@ -0,0 +1,26 @@
+/*
+ * This code is for the learning of Java
+ * It is not, and is not intended to be, production grade code.   * 
+ * Use at your own risk.  * 
+ */
+package helloworld;
+
+/**
+ *
+ * @author steve
+ */
+public class GoodByeWorld {
+    /**
+     * constructor - no scope, no return type
+     */
+    GoodByeWorld() {
+        System.out.println("Guess what, you created an instance of Good Bye world!");
+    }
+    
+    /**
+     * public method, return type of void.
+     */
+    public void sayIt(){
+        System.out.println("So, goodbye then. :-(");
+    }
+}
