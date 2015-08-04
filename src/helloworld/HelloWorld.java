69151344ca5e66b0b3956c2b9b2fd93f5de1f7b8
Steve Falcigno <steve1281@hotmail.com>
2015-08-04 11:37:43 -0400

    initial commit

---
+++ src/helloworld/HelloWorld.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld;
+
+/**
+ *
+ * @author steve
+ */
+public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Welcome back to Java Steve :-)");
+        // create an instance of GoodByWorld
+        GoodByeWorld bye = new GoodByeWorld();
+        // and then open and run
+        bye.sayIt();
+    }
+    
+}
