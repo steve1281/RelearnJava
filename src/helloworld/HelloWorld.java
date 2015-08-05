29d7d6afcc11e283a79cf92aca9f4648a04ede5b
Steve Falcigno <steve1281@hotmail.com>
2015-08-04 21:02:23 -0400

    add a Student class

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -1,9 +1,14 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package helloworld;
+
+import Chap4.Student;
+        
+
 
 /**
  *
@@ -20,6 +25,17 @@ public class HelloWorld {
         GoodByeWorld bye = new GoodByeWorld();
         // and then open and run
         bye.sayIt();
+        
+        Student firstStudent  = new Student();
+        Student secondStudent = new Student();
+        firstStudent.id = 1;
+        firstStudent.firstName = "Marc";
+        secondStudent.id = 2;
+        secondStudent.firstName  = "Sophie";
+        
+        System.out.println("second student is "+secondStudent.firstName);
+        
+                
     }
     
 }
